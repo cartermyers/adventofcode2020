769f75a79d1a9214344d75e3d24680f74e63524f
Carter Myers <cartermyers123@gmail.com>
2020-12-12 23:32:15 -0600

    Complete dec4 part 1

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,5 +1,31 @@
 (ns advent-of-code.core
   (:gen-class))
+(require '[clojure.java.io :as io])
+(require '[clojure.string :as str])
+(require '[clojure.set])
+
+(def input (slurp (.getFile (io/resource "dec4_input.txt"))))
+
+(defn parse-passport-key-fields
+  "returns all the key fields present in the passport"
+  [s]
+  (map first (map #(str/split % #":") (str/split s #"\s+"))))
+
+(defn parse-input
+  [s]
+  (map parse-passport-key-fields (str/split s #"\n\n")))
+
+(def needed_fields #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"})
+
+(defn valid-passport?
+  ([passport_fields]
+   (valid-passport? passport_fields needed_fields))
+  ([passport_fields fields]
+   (clojure.set/subset? fields (set passport_fields))))
+
+(defn count-valid-passports
+  [passports]
+  (count (filter valid-passport? passports)))
 
 
 (defn -main
