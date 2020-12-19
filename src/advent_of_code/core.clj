92bfdac91906ad71c9d161ad6e713911db3c2829
Carter Myers <cartermyers123@gmail.com>
2020-12-19 11:10:47 -0600

    Add partial solution for dec4 part 2

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -6,27 +6,52 @@
 
 (def input (slurp (.getFile (io/resource "dec4_input.txt"))))
 
-(defn parse-passport-key-fields
-  "returns all the key fields present in the passport"
+(defn parse-passport
+  "returns a map of all the fields present in the passport"
   [s]
-  (map first (map #(str/split % #":") (str/split s #"\s+"))))
+  (into {} (map #(str/split % #":") (str/split s #"\s+"))))
 
 (defn parse-input
   [s]
-  (map parse-passport-key-fields (str/split s #"\n\n")))
+  (map parse-passport (str/split s #"\n\n")))
 
 (def needed_fields #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"})
 
 (defn valid-passport?
-  ([passport_fields]
-   (valid-passport? passport_fields needed_fields))
-  ([passport_fields fields]
-   (clojure.set/subset? fields (set passport_fields))))
+  ([passport]
+   (valid-passport? passport needed_fields))
+  ([passport fields]
+   (clojure.set/subset? fields (set (map first passport)))))
 
 (defn count-valid-passports
-  [passports]
-  (count (filter valid-passport? passports)))
+  ([passports]
+   (count-valid-passports passports valid-passport?))
+  ([passports f]
+   (count (filter f passports))))
 
+; TODO: have to validate number of digits and stuff too. sigh.
+(defn valid-year?
+  [passport key start end]
+    (let [year (Integer. (get passport key))]
+      (and (<= start year) (<= year end))))
+
+(defn valid-birth?
+  [passport]
+  (valid-year? passport "byr" 1920 2002))
+
+(defn valid-issue?
+  [passport]
+  (valid-year? passport "iyr" 2010 2020))
+
+(defn valid-expiration?
+  [passport]
+  )
+
+(defn valid-passport-2?
+  [passport]
+  (reduce #(and %1 %2) 
+          (map #(% passport) [valid-birth?
+                              ])))
 
 (defn -main
   "I don't do a whole lot ... yet."
