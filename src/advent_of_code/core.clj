7f25cc6a7be4d689a7d2d81819ea709b6b61878c
Carter Myers <cartermyers123@gmail.com>
2020-12-19 12:40:21 -0600

    Complete dec6 part 1

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -3,6 +3,25 @@
 (require '[clojure.java.io :as io])
 (require '[clojure.string :as str])
 
+(def input (slurp (.getFile (io/resource "dec6_input.txt"))))
+
+
+(defn parse-group
+  "Returns a set of characters that contain the characters of each answered question"
+  [group]
+  (into #{} (filter #(not= \newline %) group)))
+
+(defn parse-input
+  [in]
+  (map parse-group (str/split in #"\n\n")))
+
+; now find sum of all yeses in groups
+
+(defn find-count
+  ([]
+   (find-count (parse-input input)))
+  ([groups]
+   (reduce + (map count groups))))
 
 (defn -main
   "I don't do a whole lot ... yet."
