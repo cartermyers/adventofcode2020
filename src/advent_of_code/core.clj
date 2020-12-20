c6ed2116aeca832a9cc833c7d7c71e2343cf3202
Carter Myers <cartermyers123@gmail.com>
2020-12-20 11:37:09 -0600

    Complete dec8 part 2

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -23,13 +23,45 @@
   ([instructions] (find-infinite-loop instructions 0 0 #{}))
   ([instructions current accumulator already_executed]
    (if (contains? already_executed current)
-     accumulator
-     (let [instruction (nth instructions current)
-           next_already_executed (conj already_executed current)]
-       (case (first instruction)
-         :nop (find-infinite-loop instructions (inc current) accumulator next_already_executed)
-         :acc (find-infinite-loop instructions (inc current) (+ accumulator (second instruction)) next_already_executed)
-         :jmp (find-infinite-loop instructions (+ current (second instruction)) accumulator next_already_executed))))))
+     nil
+     (if (= current (count instructions))
+       accumulator
+       (let [instruction (nth instructions current)
+             next_already_executed (conj already_executed current)]
+         (case (first instruction)
+           :nop (find-infinite-loop instructions (inc current) accumulator next_already_executed)
+           :acc (find-infinite-loop instructions (inc current) (+ accumulator (second instruction)) next_already_executed)
+           :jmp (find-infinite-loop instructions (+ current (second instruction)) accumulator next_already_executed)))))))
+
+
+; two parts needed to change:
+; 1) perform a search to see if changing something works. I think the easiest way is to brute force it.
+; 2) Changing to notice when the program terminates properly
+
+(defn to-jmp
+  [instruction]
+  [:jmp (second instruction)])
+
+(defn to-nop
+  [instruction]
+  [:nop (second instruction)])
+
+(defn switch-and-see-if-completes
+  [instructions instruction current]
+  (case (first instruction)
+    :nop (find-infinite-loop (assoc (vec instructions) current (to-jmp instruction)))
+    :acc nil
+    :jmp (find-infinite-loop (assoc (vec instructions) current (to-nop instruction)))))
+
+(defn search-for-error
+  "Goes through each instruction, and change it if needed"
+  ([] (search-for-error (parse-input input)))
+  ([instructions] (search-for-error instructions 0))
+  ([instructions current]
+   (let [res (switch-and-see-if-completes instructions (nth instructions current) current)]
+     (if (nil? res)
+       (search-for-error instructions (inc current))
+       res))))
 
 
 (defn -main
