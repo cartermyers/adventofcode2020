e65a8613c7269a3fff3ab327cb70b026c8226cb8
Carter Myers <cartermyers123@gmail.com>
2021-02-13 13:04:58 -0600

    Add first working implementation for dec20 part 1
    Unfortunately, I think it's too slow for the main data set :(

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,5 +1,259 @@
 (ns advent-of-code.core
   (:gen-class))
+(require '[clojure.java.io :as io])
+(require '[clojure.string :as str])
+
+(def input (slurp (.getFile (io/resource "dec20_input.txt"))))
+
+(defn parse-piece
+  "Returns a map, where the key is the x,y coordinates and the value is the puzzle thing"
+  [rows]
+  {:piece (reduce into {} (map-indexed (fn [y r] (map-indexed #(vector {:x %1 :y y} (= \# %2)) r)) rows))
+   :max-y (dec (count rows))
+   :max-x (dec (count (first rows)))})
+
+(defn parse-id
+  [id]
+  (Integer/parseInt (second (str/split id #"\s|:"))))
+
+(defn parse-tile
+  [tile]
+  (let [p (str/split tile #"\n")]
+    [(parse-id (first p))
+     (parse-piece (rest p))]))
+
+(defn parse-input
+  [in]
+  (map parse-tile (str/split in #"\n\n")))
+
+;; end of parsing
+
+(defn rotate-coordinate
+  "Rotates a coordinate counterclockwise by 90 degrees
+   Does so by translating the origin to the center of the piece,
+   then translates the pieces back."
+  [coordinate max-x max-y]
+  [(let [translated-x (- (:x (first coordinate)) (/ max-x 2))
+         translated-y (+ (- (:y (first coordinate))) (/ max-y 2))]
+     {:x (Math/round (+ (/ max-x 2)
+                         (- (* translated-x (Math/cos (/ Math/PI 2)))
+                            (* translated-y (Math/sin (/ Math/PI 2))))))
+      :y (Math/round (+ (/ max-y 2)
+                         (- (+ (* translated-x (Math/sin (/ Math/PI 2)))
+                               (* translated-y (Math/cos (/ Math/PI 2)))))))})
+   (second coordinate)])
+
+(defn transform-piece-with-id
+  "Helper function for things like roatate and fil
+   because they want to modify the piece but use max-x and max-y 
+   (which) should never change"
+  [piece-with-id f]
+  [(first piece-with-id)
+   (let [piece (second piece-with-id)]
+     {:piece (into {} (keep f (:piece piece)))
+      :max-x (:max-x piece)
+      :max-y (:max-y piece)})])
+
+(defn rotate
+  [piece-with-id]
+  (let [piece (second piece-with-id)]
+    (transform-piece-with-id piece-with-id #(rotate-coordinate % (:max-x piece) (:max-y piece)))))
+
+(defn get-all-rotations
+  "For a given piece, returns a list of that piece rotated 90 degrees at a time."
+  [piece-with-id]
+  (loop [count 0
+         res (list piece-with-id)]
+    (if (>= count 3)
+      res
+      (recur (inc count)
+             (conj res (rotate (first res)))))))
+
+(defn flip-x
+  [coordinate max-x]
+  [(update (first coordinate) :x #(- max-x %))
+   (second coordinate)])
+
+(defn flip
+  "Only need to rotate around the y-axis, and all of the rotations will handle the rest."
+  [piece-with-id]
+  (transform-piece-with-id piece-with-id #(flip-x % (:max-x (second piece-with-id)))))
+
+(defn get-top-edge
+  [piece]
+  ;get all coordinates for y=0
+  (into {} (filter #(= 0 (:y (first %))) (:piece piece))))
+
+(defn do-top-edges-match?
+  "If the two top edges match, return the piece ID of the second piece only"
+  [piece1-with-id piece2-with-id]
+  (if (= (get-top-edge (second piece1-with-id))
+         (get-top-edge (second piece2-with-id)))
+    (first piece2-with-id)
+    nil))
+
+(defn matches-top-edge?
+  "Returns the piece ID of the second arguement if any edge of piece2 matches the top of piece1"
+  [piece1-with-id piece2-with-id]
+    (let [rotations (get-all-rotations piece2-with-id)]
+      (some #(do-top-edges-match? piece1-with-id %) (concat rotations (map flip rotations))))
+  )
+
+(defn pieces-match?
+  "For these two pieces, we need to find two edges that match.
+   Given an edge of piece1, any edge in piece2 can match (if flipped or rotated)
+   And then it would need to be rotated"
+  [piece1-with-id piece2-with-id]
+  (if (= (first piece1-with-id) (first piece2-with-id)) ; we won't let pieces match themselves. if they have the same id, then it's nothing
+    nil
+    (some #(matches-top-edge? % piece2-with-id) (get-all-rotations piece1-with-id))))
+
+(defn get-piece-matches
+  "Currently takes full O(n^2) because I check all of the pieces in the bag.
+   We could stop once a piece has 4 edges. And if we account for all its partners, 
+   then we can even remove that piece from future checks.
+   But I think this is not needed yet."
+  [piece-with-id all-pieces-with-ids]
+  (keep #(pieces-match? piece-with-id %) all-pieces-with-ids))
+
+(defn is-edge?
+  [val max-x max-y]
+  (let [coordinate (first val)]
+    (if (or (= 0 (:x coordinate))
+            (= 0 (:y coordinate))
+            (= max-x (:x coordinate))
+            (= max-y (:y coordinate)))
+      val
+      nil)))
+
+(defn remove-insides
+  [piece-with-id]
+  (transform-piece-with-id piece-with-id #(is-edge? % (:max-x (second piece-with-id)) (:max-y (second piece-with-id)))))
+
+(defn find-piece-matches
+  ([all-pieces]
+   (let [pieces-with-only-edges (map remove-insides all-pieces)]
+     (into {} (map #(vector (first %) (get-piece-matches % pieces-with-only-edges)) pieces-with-only-edges)))))
+
+; we only need to find the edges, so those are 4 pieces that only have 2 matching pieces in the bag.
+; hopefully there aren't multiple matches (i.e., a single piece can work for more than 4 pieces or something)
+; that would be a shitty puzzle
+
+(defn solve-part1
+  ([] (solve-part1 (parse-input input)))
+  ([all-pieces] 
+   (reduce * (map first (filter #(= 2 (count (second %))) (find-piece-matches all-pieces))))))
+
+
+(def ex "Tile 2311:
+..##.#..#.
+##..#.....
+#...##..#.
+####.#...#
+##.##.###.
+##...#.###
+.#.#.#..##
+..#....#..
+###...#.#.
+..###..###
+
+Tile 1951:
+#.##...##.
+#.####...#
+.....#..##
+#...######
+.##.#....#
+.###.#####
+###.##.##.
+.###....#.
+..#.#..#.#
+#...##.#..
+
+Tile 1171:
+####...##.
+#..##.#..#
+##.#..#.#.
+.###.####.
+..###.####
+.##....##.
+.#...####.
+#.##.####.
+####..#...
+.....##...
+
+Tile 1427:
+###.##.#..
+.#..#.##..
+.#.##.#..#
+#.#.#.##.#
+....#...##
+...##..##.
+...#.#####
+.#.####.#.
+..#..###.#
+..##.#..#.
+
+Tile 1489:
+##.#.#....
+..##...#..
+.##..##...
+..#...#...
+#####...#.
+#..#.#.#.#
+...#.#.#..
+##.#...##.
+..##.##.##
+###.##.#..
+
+Tile 2473:
+#....####.
+#..#.##...
+#.##..#...
+######.#.#
+.#...#.#.#
+.#########
+.###.#..#.
+########.#
+##...##.#.
+..###.#.#.
+
+Tile 2971:
+..#.#....#
+#...###...
+#.#.###...
+##.##..#..
+.#####..##
+.#..####.#
+#..#.#..#.
+..####.###
+..#.#.###.
+...#.#.#.#
+
+Tile 2729:
+...#.#.#.#
+####.#....
+..#.#.....
+....#..#.#
+.##..##.#.
+.#.####...
+####.#.#..
+##.####...
+##..#.##..
+#.##...##.
+
+Tile 3079:
+#.#.#####.
+.#..######
+..#.......
+######....
+####.#..#.
+.#...#.##.
+#.#####.##
+..#.###...
+..#.......
+..#.###...")
+
+(def ex-all (parse-input ex))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/advent_of_code/core_test.clj
+++ test/advent_of_code/core_test.clj
@@ -1,3 +1,91 @@
 (ns advent-of-code.core-test
   (:require [clojure.test :refer :all]
             [advent-of-code.core :refer :all]))
+
+(defn get-max
+  [coords axis]
+  (axis (first (apply max-key #(axis (key %)) coords))))
+
+(defn piece-with-id-maxes
+  ([id coords max-x max-y] [id {:piece coords :max-x max-x :max-y max-y}])
+  ([id coords] (piece-with-id-maxes id coords (get-max coords :x) (get-max coords :y)))
+  ([coords] (piece-with-id-maxes 1234 coords)))
+
+(defn piece-with-id
+  ([id coords] [id {:piece coords}])
+  ([coords] (piece-with-id 1234 coords)))
+
+(deftest test-get-top-edge
+  (testing "Get top edge empty"
+    (is (= {} (get-top-edge {:piece {}}))))
+  (testing "Get top edge all y=0"
+    (is (= {{:x 1 :y 0} true {:x 4 :y 0} false}
+           (get-top-edge {:piece {{:x 1 :y 0} true
+                                  {:x 4 :y 0} false}}))))
+    (testing "Get top edge mixed"
+      (is (= {{:x 1 :y 0} true {:x 4 :y 0} false}
+             (get-top-edge {:piece {{:x 1 :y 0} true
+                                    {:x 4 :y 0} false
+                                    {:x 34 :y 7} false
+                                    {:x 65 :y 1} true}})))))
+
+(deftest test-do-top-edges-match?
+  (testing "Empty pieces"
+    (is (do-top-edges-match? (piece-with-id {}) (piece-with-id {}))))
+  (testing "Match returns id of second piece"
+    (is (= 2 (do-top-edges-match? (piece-with-id 1 {}) (piece-with-id 2 {})))))
+  (testing "Pieces with disjoint coordinates don't match"
+    (is (not (do-top-edges-match? (piece-with-id {{:x 1 :y 0} false}) 
+                                  (piece-with-id {{:x 2 :y 0} false})))))
+  (testing "Pieces with different values don't match"
+    (is (not (do-top-edges-match? (piece-with-id {{:x 1 :y 0} false}) 
+                                  (piece-with-id {{:x 1 :y 0} true})))))
+  (testing "Pieces match"
+    (is (do-top-edges-match? (piece-with-id {{:x 1 :y 0} false}) 
+                             (piece-with-id {{:x 1 :y 0} false})))))
+
+
+(deftest test-matches-top-edge?
+  (testing "Empty pieces"
+    (is (matches-top-edge? (piece-with-id {}) (piece-with-id {}))))
+  (testing "Match returns id of second piece"
+    (is (= 2 (matches-top-edge? (piece-with-id 1 {}) (piece-with-id 2 {})))))
+  (testing "Match same top edge"
+    (is (matches-top-edge? (piece-with-id-maxes {{:x 0 :y 0} true}) 
+                           (piece-with-id-maxes {{:x 0 :y 0} true}))))
+  (testing "Match same top edge, flipped"
+    (is (matches-top-edge? (piece-with-id-maxes {{:x 0 :y 0} true {:x 1 :y 0} false})
+                           (piece-with-id-maxes {{:x 0 :y 0} false {:x 1 :y 0} true}))))
+  (testing "Match same top edge, rotated"
+    (is (matches-top-edge? (piece-with-id-maxes {{:x 0 :y 0} true})
+                           (piece-with-id-maxes {{:x 0 :y 1} true}))))
+  (testing "Match same top edge, rotated and flipped"
+    (is (matches-top-edge? (piece-with-id-maxes {{:x 0 :y 0} true {:x 1 :y 0} false})
+                           (piece-with-id-maxes {{:x 0 :y 1} false {:x 1 :y 1} true})))))
+
+(deftest test-pieces-match?
+  (testing "Empty pieces"
+    (is (pieces-match? (piece-with-id 1 {}) (piece-with-id 2 {}))))
+  (testing "Pieces with same ID don't match"
+    (is (not (pieces-match? (piece-with-id 1 {}) (piece-with-id 1 {})))))
+  (testing "Match returns ID of second piece"
+    (is (= 2 (pieces-match? (piece-with-id 1 {}) (piece-with-id 2 {})))))
+  (testing "Bottom edge of piece 1 matches top of piece 2"
+    (is (pieces-match? (piece-with-id-maxes 1 {{:x 0 :y 4} true {:x 1 :y 4} false})
+                       (piece-with-id-maxes 2 {{:x 0 :y 0} true {:x 1 :y 0} false
+                                               {:x 1 :y 4} true})))) ; add one coordinate with y=4 for proper translations
+  (testing "Left edge of piece 1 matches right of piece 2"
+    (is (pieces-match? (piece-with-id-maxes 1 {{:x 0 :y 0} true {:x 0 :y 1} true {:x 0 :y 2} true
+                                               {:x 2 :y 1} false}) ; add x=2 for proper translations
+                       (piece-with-id-maxes 2 {{:x 2 :y 0} true {:x 2 :y 1} true {:x 2 :y 2} true}))))
+  (testing "Pieces don't match"
+    (is (not (pieces-match? (piece-with-id-maxes 1 {{:x 0 :y 0} true {:x 0 :y 1} true {:x 0 :y 2} true
+                                                    {:x 2 :y 1} false}) ; add x=2 for proper translations
+                            (piece-with-id-maxes 2 {{:x 2 :y 0} false {:x 2 :y 1} false {:x 2 :y 2} false}))))))
+
+(deftest test-get-piece-matches
+  (testing "Piece does not match with no pieces"
+    (is (= '() (get-piece-matches (piece-with-id {}) '()))))
+  (testing "Empty pieces match"
+    (is (= '(2 3) (get-piece-matches (piece-with-id 1 {}) 
+                                  (list (piece-with-id 2 {}) (piece-with-id 3 {})))))))
